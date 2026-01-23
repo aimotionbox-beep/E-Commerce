a42ee57c29f5ab4ffd6ce0d3f62053185b6e2b16
Nurul Islam <gnbsml2018sni.013@gmail.com>
2026-01-23 17:22:14 +0530

    change in server.js in cors for token

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
+    "Content-Type, Authorization, token"
   );
 
   // ✅ Handle preflight requests
